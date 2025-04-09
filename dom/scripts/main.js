cecc0f31b4131603b035e2941ba91a282d41d787
imShiryo <balint4.official@gmail.com>
2025-04-09 18:23:25 +0200

    done

--- dom/scripts/main.js
+++ dom/scripts/main.js
@@ -1,14 +1,35 @@
 const btnGroup = document.querySelector('.btn-group');
+const prevBtn = btnGroup.querySelector('button.btn:nth-child(1)');
+const numBtn = btnGroup.querySelector('button.btn:nth-child(2)');
+const nextBtn = btnGroup.querySelector('button.btn:nth-child(3)');
 
-const prevBtn = document.querySelector('button.btn:nth-child(1)');
+let counter = 1;
+const setNumBtn = (num = 1) => {
+    numBtn.innerHTML = num;
+}
+
 if (prevBtn){
     prevBtn.classList.add('btn-info');
 }
-const numBtn = document.querySelector('button.btn:nth-child(2)');
+
 if (numBtn){
     numBtn.classList.add('btn-primary');
 }
-const nextBtn = document.querySelector('button.btn:nth-child(3)');
+
 if (nextBtn){
     nextBtn.classList.add('btn-info');
 }
+
+nextBtn.addEventListener('click', (e)=>{
+    counter++;
+    setNumBtn(counter)
+});
+
+prevBtn.addEventListener('click', (e) => {
+    if (counter < 2){
+        return;
+    }
+    
+    counter--;
+    setNumBtn(counter);
+});
